3b3b550c073918d1e3577d58a05affb82387afa5
Digital Villa <digitalvilla01@gmail.com>
2020-01-08 23:22:59 -0700

    Initial commit | Added initial folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sass/styles.scss'
+import HomePage from './pages/Home';
+
+function App() {
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../sass/pages/home.scss';
+import Directory from '../components/Directory';
+
+const HomePage = () => (
+	<div className='homepage'>
+		<Directory />
+	</div>
+);
+
+export default HomePage;
